bfff225f164deb0536c526ef70a444f7ed2ff9c9
jakub jilek <jilek@oksystem.cz>
2020-04-07 19:14:28 +0200

    implementován routerLink, základ pro herní okno

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -8,13 +7,9 @@ import {Router} from '@angular/router';
 })
 export class MenuComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  newGame() {
-    this.router.navigate(['/new-hero-form']);
   }
 
   endGame() {
