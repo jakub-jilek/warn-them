020895c3d505d97ba124c55e841eea1b0a42e345
jakub jilek <jilek@oksystem.cz>
2020-04-10 23:00:29 +0200

    Okno rozděleno na main a hero část

--- src/app/game/game-window/game-window.component.ts
+++ src/app/game/game-window/game-window.component.ts
@@ -3,14 +3,14 @@ import {HeroService} from 'src/app/hero/hero.service';
 
 @Component({
   selector: 'game-window',
-  templateUrl: './game-window.component.html'
+  templateUrl: './game-window.component.html',
+  styleUrls: ['game-window.component.css']
 })
 export class GameWindowComponent implements OnInit {
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-    console.log('GAME WINDOW ', this.heroService.hero);
   }
 
 }
